f795d14a8ce9915ae6fdf2717bbd73d141196456
Ebrahim Salhi Panah <65587143+ebrahimsalehipanah@users.noreply.github.com>
2021-04-16 16:35:17 +0430

    finish :)

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 		let minuteHand = document.getElementsByClassName("mn")[0];
 		let secondHand = document.getElementsByClassName("sc")[0];
 
-        let time = new Date();
+		let time = new Date();
 		let hour = time.getHours();
 		let minute = time.getMinutes();
-		let second = time.getSeconds();
+		let second = time.getSeconds(); 
 
-		hourHand.style.transform = `rotate(${hourToDeg(hour) + second / 10}deg)`;
+		hourHand.style.transform = `rotate(${hourToDeg(hour, minute)}deg)`;
 		minuteHand.style.transform = `rotate(${toDeg(minute) + second / 10}deg)`;
 		secondHand.style.transform = `rotate(${toDeg(second)}deg)`;
 
@@ -18,9 +18,9 @@
 			return (parseInt(secOrMin) - 15) * 6;
 		}
 
-		function hourToDeg(hour, min, sec) {
+		function hourToDeg(hour, min) {
 			// 60 min = 1 hour = 30deg ==> 2 min = 1 deg
-			return (parseInt(hour) - 3) * 30 + parseInt(min)/2;
+			return (parseInt(hour) - 3) * 30 + parseInt(min) / 2;
 		}
 	}, 1000);
 })();
